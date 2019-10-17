11deae60831a8c2dee08f0920432ad8bd3e94c1e
ekaterinatretyak <45742265+ekaterinatretyak@users.noreply.github.com>
2019-10-17 09:29:27 +0300

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -8,23 +8,23 @@ def letters(s):
     l = []  # Define an array
     # Creates a tuple(index,character)
     for j,c in enumerate(s):
-        # if there's an alphabetical character
-        # and its index isn't more than 0 or the previous character isn't alpha
-        # write it as a beginning of a substring
+        # If there's an alphabetical character
+        # And its index isn't more than 0 or the previous character isn't alpha
+        # Write it as a beginning of a substring
         if c.isalpha():
             if not((j > 0) and (s[j-1].isalpha())):
                 i = j
-        # if there is a non-alphabetical character
-        # and its index is more than 0 and the previous one is alpha
-        # it means that there is the end of the substring
-        # if the previous character is also non-alpha, we move on
+        # If there is a non-alphabetical character
+        # And its index is more than 0 and the previous one is alpha
+        # It means that there is the end of the substring
+        # If the previous character is also non-alpha, we move on
         else:
             if (s[j-1].isalpha()) and (j > 0):
                     l.append(s[i:j])
             if not (s[j-1].isalpha()):
                     i = j + 1
-    # while i is less than a string's length
-    # add to the array a slice from i to j
+    # While i is less than a string's length
+    # Add to the array a slice from i to j
     if i < len(s):
         l.append(s[i:])
     return l
