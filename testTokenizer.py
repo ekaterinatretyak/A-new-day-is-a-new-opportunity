ae19b5307412379ae5c69533517204015c4e1338
ekaterinatretyak <45742265+ekaterinatretyak@users.noreply.github.com>
2019-11-13 22:57:00 +0300

    Add files via upload

---
+++ testTokenizer.py
@@ -0,0 +1,61 @@
+import unittest
+import myTokenizer
+
+class TestMyTokenizer(unittest.TestCase):
+
+    def test_tokenize(self):
+        s = 'Я не играю'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я! не, 0играю'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я98 не 14 играю'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я !9 не) 43играю'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я не играю90'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я не играю!'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+        
+
+class TestStringMethods(unittest.TestCase):
+    
+    def test_tokenize(self):
+        s = 'Я не играю'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+     
+    def test_tokenize(self):
+        s = 'Я! не, 0играю'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я98 не 14 играю'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я !9 не) 43играю'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я не играю90'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+    def test_tokenize(self):
+        s = 'Я не играю!'
+        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+
+
+if __name__ == '__main__':
+        unittest.main()
+                         
+
