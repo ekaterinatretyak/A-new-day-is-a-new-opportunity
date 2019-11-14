2a92cb92aa426d7c0545c75f23d7d0ff0653b691
ekaterinatretyak <45742265+ekaterinatretyak@users.noreply.github.com>
2019-11-14 09:32:29 +0300

    Update testTokenizer.py

--- testTokenizer.py
+++ testTokenizer.py
@@ -2,36 +2,34 @@ import unittest
 import myTokenizer
 
 class TestMyTokenizer(unittest.TestCase):
+    
+    def setUp(self):
+        self.t = MyTokenizer()
 
     def test_tokenize(self):
         s = 'Я не играю'
-        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+        self.assertEqual(self.t.tokenize(s), ['Я', 'не', 'играю'])
 
     def test_tokenize(self):
         s = 'Я! не, 0играю'
-        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+        self.assertEqual(self.t.tokenize(s), ['Я', 'не', 'играю'])
 
     def test_tokenize(self):
         s = 'Я98 не 14 играю'
-        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+        self.assertEqual(self.t.tokenize(s), ['Я', 'не', 'играю'])
 
     def test_tokenize(self):
         s = 'Я !9 не) 43играю'
-        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+        self.assertEqual(self.t.tokenize(s), ['Я', 'не', 'играю'])
 
     def test_tokenize(self):
         s = 'Я не играю90'
-        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
+        self.assertEqual(self.t.tokenize(s), ['Я', 'не', 'играю'])
        
     def test_tokenize(self):
         s = ''
-        self.assertEqual(myTokenizer.tokenize(s), [])
-
-    def test_tokenize(self):
-        s = 'Я не играю!'
-        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
-        
-
+        self.assertEqual(self.t.tokenize(s), [])
+      
 class TestStringMethods(unittest.TestCase):
     
     def test_tokenize(self):
@@ -57,10 +55,6 @@ class TestStringMethods(unittest.TestCase):
     def test_tokenize(self):
         s = ''
         self.assertEqual(alpha.letters(s), [])
-
-    def test_tokenize(self):
-        s = 'Я не играю!'
-        self.assertEqual(myTokenizer.tokenize(s), ['Я', 'не', 'играю'])
 
 
 if __name__ == '__main__':
