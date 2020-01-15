55b6e1210a7b56829f82a69b674b602d10850403
ekaterinatretyak <45742265+ekaterinatretyak@users.noreply.github.com>
2020-01-15 12:11:07 +0300

    Update myTokenizer.py

--- myTokenizer.py
+++ myTokenizer.py
@@ -2,78 +2,9 @@
 of alphabetical substrings from an arbitrary string"""
 
 import unicodedata
-
-class MyTokenizer(object):
-    """This class tokenizes a string based on the method 'tokenize'"""
-    def get_tokens_by_categories(self, s):
-        cat = {'SPACE': [],
-                'ALPHA': [],
-                'DIGIT': [],
-               'UNKNOWN': []}
-        for c in s:
-                if c.isspace():
-                    cat['SPACE'].append(c)
-                if c.isalpha():
-                    cat['ALPHA'].append(c)
-                if c.isdigit():
-                    cat['DIGIT'].append(c)
-                else:
-                    cat['UNKNOWN'].append(c)
-                return cat
-    def tokenize_all_substrings(self, s):
-        if len(s) == 0:
-            l = []
-        words = []
-        index = 0
-        cat = self.get_tokens_by_categories(s[0])
-        for j, c in enumerate(s):
-            current_cat = self.get_tokens_by_categories(s)
-            if current_cat != self.get_tokens_by_categories(s[j-1]) and j>0:
-                token = s[index:j]
-                k = Token(token, index, cat)
-                words.append(k)
-                index = j
-                cat = current_cat
-        if index < len(s):
-            token = s[index:]
-            k = Token(token, index, cat)
-            words.append(k)
-        return words
-    
-    def tokenize(self, s):
-        """This method takes a string as argument and splits it alphabetical
-        substrings from the space to space"""
-        i = 0
-        l = [] # An array for our future list of substrings
-        if len(s) == 0:
-            l = []
-        # Check each object(index,character) whether it is alpha or not
-        for j,c in enumerate(s):
-            # If there's an alphabetical character
-            # it can be the first char in string
-            # or the previous character isn't alpha
-            # Write it as a beginning of a substring
-            if c.isalpha():
-                if not(((j > 0) or (s[j-1].isalpha()))):
-                    i = j
-            # Otherwise, we move on
-            else:
-                i = j + 1
-                
-            # Check whether it is not the last char in the string
-            if (j+1 <= len(s)-1):
-                # Find the end of alpha substring and add it to list
-                if c.isalpha() and not s[j+1].isalpha():
-                    l.append(s[i:j+1])
-            
-        # The part below finds the last char in the string
-        # and adds it to the array if it is alpha
-
-        if c.isalpha():
-            l.append(s[i:])
-            
-        return l
 def tokenize(s):
+    """This function divides a string in a list of alphabetical substrings."""
+    
     i = 0
     l = [] # An array for our future list of substrings
     if len(s) == 0:
@@ -102,24 +33,127 @@ def tokenize(s):
         l.append(s[i:])
             
     return l
-        
+
+class MyTokenizer(object):
+    """This class tokenizes a string based on the method 'tokenize'"""
     
+    def tokenize(self, s):
+        """This method takes a string as argument and splits it alphabetical
+        substrings from the space to space"""
+        i = 0
+        l = [] # An array for our future list of substrings
+        if len(s) == 0:
+            l = []
+        # We go through each char in the string
+        # and check each object(index,character) whether it is alpha or not
+        for j,c in enumerate(s):
+            # If there's an alphabetical character
+            # it can be the first char in string
+            # or the previous character isn't alpha
+            # Write it as a beginning of a substring
+            if c.isalpha():
+                if not(((j > 0) or (s[j-1].isalpha()))):
+                    i = j
+            # Otherwise, we move on
+            else:
+                i = j + 1
+                
+            # Check whether it is not the last char in the string
+            if (j+1 <= len(s)-1):
+                # Find the end of alpha substring and add it to list
+                if c.isalpha() and not s[j+1].isalpha():
+                    l.append(s[i:j+1])
+            
+        # The part below finds the last char in the string
+        # and adds it to the array if it is alpha
+
+        if c.isalpha():
+            l.append(s[i:])
+            
+        return l
+    
+    @staticmethod
+    def get_tokens_by_categories(char):
+        """This method determines categories of chars in the string
+        and adds chars to the arrays"""
+        
+        cat = {'SPACE': [],
+                'ALPHA': [],
+                'DIGIT': [],
+                'PUNCTUATION': [],
+                'UNKNOWN': []}
+        
+        if c.isspace():   
+            cat['SPACE'].append(c)
+        if c.isalpha():
+            cat['ALPHA'].append(c)
+        if c.isdigit():
+            cat['DIGIT'].append(c)
+        if unicodedata.category(c)[0] == 'P':
+            cat['PUNCTUATION'].append(c)
+        else:
+            cat['UNKNOWN'].append(c)
+        return cat
+            
+    def tokenize_categories(self, s):
+        """This method returns token, its category and
+        index of the 1st and the last char os substring in the user's string"""
+        
+        words = []
+        if len(s) == 0:
+            words = []
+        # We go through each char in the string
+        # and check each object(index,character) whether it is alpha or not
+        for j,c in enumerate(s):
+            
+        cat = self.get_tokens_by_categories(s[0])
+        for j, c in enumerate(s):
+            category = MyTokenizer.get_tokens_by_categories(c)
+            if i == 0:
+                index = i
+                prevcat = category
+            # Check whether it is not the last char in the string
+            if (i+1 <= len(s)-1):
+                # We compare categories of current and the previous characters
+                # if they differ, it is the the last char of the token
+                if category != prevcat:
+                    token = s[index:i]
+                    k = Token(token, prevcat, index, i)
+                    words.append(k)
+                    index = i
+                    prevcat = category
+         # The part below finds the last char in the string
+         # and checks it
+            token = s[index:]
+            i = i + 1  
+            k = Token(token, category, index, i) 
+            words.append(k)         
+        return data2
+         
+        
 class Token(object):
-    def __init__(self, token, index, cat):
-        self.token = token
-        self.index = index
-        self.cat = cat
-    def __repr__(self):
-        return self.token + ":" + s(self.index) + s(self.cat)
+    """All the substrings belong to the class 'Token'.
+    This class represents the next information about a token:
+    token: the token itself.
+    firstind: the position of the first character.
+    lastind: the position of the last character.
+    cat: the category of the token"""
+    
+   def __init__(self, token, cat, firstind, lastind):      
+         self.firstindex = firstind
+         self.lastindex = lastind
+         self.token = token
+         self.category = cat
+            
+   def __repr__(self):
+         """Returns the object representation"""
+        return ' ' + self.token + ' is  ' + self.category + ' located from ' + s(self.firstindex) + ' index to ' + s(self.lastindex) + ' index.' + '\n'
 
-
+    
 def main():
-    t = MyTokenizer()
+    t = myTokenizer()
     s = input()
-    print(t.tokenize(s))
-    print(tokenize(s))
-    print(t.tokenize_all_substrings(s))
-   
+    print(t.tokenize_categories(s))
 
 if __name__ == '__main__':
     main()
